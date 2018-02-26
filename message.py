bcba114a3752ed35daed7d2876815036507bb310
dim_lekk <dimlek.ece@gmail.com>
2018-02-27 01:01:09 +0200

    Helper class for message ordering functionality.

---
+++ message.py
@@ -0,0 +1,10 @@
+
+
+class Message:
+
+    def __init__(self, message_content, group_name, username, serial_no=0):
+        self.message_content = message_content
+        self.group_name = group_name
+        self.username = username
+        self.serial_no = serial_no
+
